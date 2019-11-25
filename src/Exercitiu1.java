99416e3e006dcdaf65cd98e901793c2ca5e15b4f
Liudmila <mehedinteanuliudmila@yahoo.com>
2019-11-25 12:07:56 +0200

    import project

---
+++ src/Exercitiu1.java
@@ -0,0 +1,5 @@
+public class Exercitiu1 {
+    public static void main(String[] args) {
+        System.out.println("Hello Liudmila Ciobanu");
+    }
+}
---
+++ src/Exercitiu2.java
@@ -0,0 +1,8 @@
+public class Exercitiu2 {
+    public static void main(String[] args) {
+        System.out.println(74 + 36);
+
+    }
+
+
+}
