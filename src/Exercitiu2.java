29f518afa0f3cb615927b2c22868b4c212d4f736
Liudmila <mehedinteanuliudmila@yahoo.com>
2019-11-25 13:22:48 +0200

    import project

--- src/Exercitiu2.java
+++ src/Exercitiu2.java
@@ -1,7 +1,14 @@
 public class Exercitiu2 {
     public static void main(String[] args) {
-        System.out.println(74 + 36);
+        System.out.println("Introduceti numarul: ");
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int i = 1;
+        while (i <= 9) {
+            System.out.println(i + "*" + n + "=" + i*n);
+            i++;
 
+        }
     }
 
 
