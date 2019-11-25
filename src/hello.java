7d3969e423d56e8e6bae1ffb1cc66c1811afb01c
Liudmila <mehedinteanuliudmila@yahoo.com>
2019-11-25 11:59:33 +0200

    import project

---
+++ src/hello.java
@@ -0,0 +1,9 @@
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello Java!");
+        System.out.println("hello Java!");
+        long age = 12567890;
+        boolean lightOn = false;
+        System.out.println(age);
+    }
+}
