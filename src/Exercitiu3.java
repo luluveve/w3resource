4207b6bf97a27f057590c6c96303ce7a699c13f9
Liudmila <mehedinteanuliudmila@yahoo.com>
2019-11-26 12:50:28 +0200

    import project

---
+++ src/Exercitiu3.java
@@ -0,0 +1,8 @@
+public class Exercitiu3 {
+    public static void main(String[] args) {
+        System.out.println("J    a   v     v  a");
+        System.out.println("J   a a  v     v  a");
+        System.out.println("J  J  aaaaa   V V  aaaaa");
+        System.out.println("JJ  a     a   V  a     a");
+    }
+}
