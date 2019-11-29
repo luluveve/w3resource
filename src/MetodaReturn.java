2ba305decb0c5554502e0a521833be720ce0b401
Liudmila <mehedinteanuliudmila@yahoo.com>
2019-11-29 14:35:44 +0200

    import project

---
+++ src/MetodaReturn.java
@@ -0,0 +1,9 @@
+public class MetodaReturn {
+    public static int afiseaza (int x) {
+        return 5 + x;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(afiseaza (3));
+    }
+}
